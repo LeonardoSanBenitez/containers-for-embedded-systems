de5bd501f899a7042c1f9ab85ee8243c8c3f1228
Leonardo Benitez <lsbenitezpereira@gmail.com>
2021-12-17 23:28:04 -0300

    blinking leds from both containers

---
+++ init-os.sh
@@ -0,0 +1,5 @@
+# Install the OS from the Nvidia’s official website:
+# https://developer.nvidia.com/jetson-nano-sd-card-image
+
+sudo apt-get install -y libhdf5-dev libssl-dev python3 python3-pip
+sudo pip3 install docker-compose==1.27.4
