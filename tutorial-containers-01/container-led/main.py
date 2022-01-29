bdc70c7d0cdd11256e88363796e203e02d7bd733
Leonardo Benitez <lsbenitezpereira@gmail.com>
2022-01-29 11:14:59 -0300

    basic webserver

--- tutorial-containers-01/container-led/main.py
+++ tutorial-containers-01/container-led/main.py
@@ -1,9 +1,18 @@
-import time
 import qwiic_button
+import fastapi
+import uvicorn
 
-my_button = qwiic_button.QwiicButton()
-while True:
-    my_button.LED_on(50)
-    time.sleep(0.3)
-    my_button.LED_off()
-    time.sleep(0.3)
+app = fastapi.FastAPI()
+
+@app.post("/{state}")
+async def root(state: bool) -> None:
+    if state==True:
+        app.state.my_button.LED_on(255)
+    else:
+        app.state.my_button.LED_off()
+    
+@app.on_event('startup')
+async def server_startup():
+    app.state.my_button = qwiic_button.QwiicButton()
+
+uvicorn.run('main:app', host="0.0.0.0", port=80, reload=True, debug=True)
